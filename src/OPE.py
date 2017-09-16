786aefbdfd9db5fd26b14627ec1e228df8be89dc
markozeman <mz4730@student.uni-lj.si>
2017-09-16 21:17:47 +0200

    GUI without functionality

--- src/OPE.py
+++ src/OPE.py
@@ -3,7 +3,8 @@ from collections import OrderedDict
 
 from shutil import copy
 
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QButtonGroup, QSpacerItem, QVBoxLayout, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QButtonGroup, QSpacerItem, QVBoxLayout, QSizePolicy, \
+    QLabel, QLineEdit
 
 from help_functions import *
 
@@ -204,8 +205,6 @@ class OPE:
         [os.remove(dec_path + f) for f in dec_files]
 
 
-class QString(object):
-    pass
 
 
 class SearhGUI(QWidget):
@@ -213,9 +212,8 @@ class SearhGUI(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.height = 700
+        self.height = 480
         self.width = 720
-
 
         self.setGeometry(700, 150, self.width, self.height)
         self.setWindowTitle('SSE')
@@ -232,49 +230,123 @@ class SearhGUI(QWidget):
         left_bracket.move(hor_spacing, hor_spacing)
         right_bracket.move(ver_spacing+hor_spacing, hor_spacing)
 
-        self.horizontal_line(0, 3*hor_spacing)
+        self.horizontal_line(3*hor_spacing)
 
         properties_widget = QWidget(self)
         properties_widget.move(hor_spacing, 4*hor_spacing)
         properties_button_group = QButtonGroup()
         birth_date = QPushButton('birthDate', properties_widget)
+        properties_button_group.addButton(birth_date)
         first_name = QPushButton('firstName', properties_widget)
         first_name.move(ver_spacing, 0)
+        properties_button_group.addButton(first_name)
         last_name = QPushButton('lastName', properties_widget)
         last_name.move(2*ver_spacing, 0)
+        properties_button_group.addButton(last_name)
         street = QPushButton('street', properties_widget)
         street.move(3*ver_spacing, 0)
+        properties_button_group.addButton(street)
         house_number = QPushButton('houseNum', properties_widget)
         house_number.move(4*ver_spacing, 0)
+        properties_button_group.addButton(house_number)
         city = QPushButton('city', properties_widget)
         city.move(5*ver_spacing, 0)
+        properties_button_group.addButton(city)
         post_code = QPushButton('postCode', properties_widget)
         post_code.move(6*ver_spacing, 0)
+        properties_button_group.addButton(post_code)
         country = QPushButton('country', properties_widget)
         country.move(0, 2*hor_spacing)
+        properties_button_group.addButton(country)
         type_of = QPushButton('type', properties_widget)
         type_of.move(ver_spacing, 2 * hor_spacing)
+        properties_button_group.addButton(type_of)
         temperature = QPushButton('temperature', properties_widget)
         temperature.move(2*ver_spacing, 2*hor_spacing)
+        properties_button_group.addButton(temperature)
         heart_rate = QPushButton('heartRate', properties_widget)
         heart_rate.move(3*ver_spacing, 2*hor_spacing)
+        properties_button_group.addButton(heart_rate)
         diastolic = QPushButton('diastolic', properties_widget)
         diastolic.move(4*ver_spacing, 2*hor_spacing)
+        properties_button_group.addButton(diastolic)
         systolic = QPushButton('systolic', properties_widget)
         systolic.move(5*ver_spacing, 2*hor_spacing)
+        properties_button_group.addButton(systolic)
         spO2 = QPushButton('spO2', properties_widget)
         spO2.move(6*ver_spacing, 2*hor_spacing)
+        properties_button_group.addButton(spO2)
 
-        self.horizontal_line(0, 8*hor_spacing)
+        self.horizontal_line(8*hor_spacing)
+
+        operators_widget = QWidget(self)
+        operators_widget.move(hor_spacing, 9*hor_spacing)
+        operators_button_group = QButtonGroup()
+        eq = QPushButton('=', operators_widget)
+        operators_button_group.addButton(eq)
+        ne = QPushButton('≠', operators_widget)
+        ne.move(ver_spacing, 0)
+        operators_button_group.addButton(ne)
+        lt = QPushButton('<', operators_widget)
+        lt.move(2*ver_spacing, 0)
+        operators_button_group.addButton(lt)
+        lte = QPushButton('≤', operators_widget)
+        lte.move(3*ver_spacing, 0)
+        operators_button_group.addButton(lte)
+        gt = QPushButton('>', operators_widget)
+        gt.move(4*ver_spacing, 0)
+        operators_button_group.addButton(gt)
+        gte = QPushButton('≥', operators_widget)
+        gte.move(5*ver_spacing, 0)
+        operators_button_group.addButton(gte)
+
+        self.horizontal_line(11*hor_spacing)
+
+        value_widget = QWidget(self)
+        value_widget.move(hor_spacing, 12*hor_spacing)
+        val_label = QLabel('Value: ', value_widget)
+        val_label.move(0, 4)
+        val_line_edit = QLineEdit(value_widget)
+        val_line_edit.setFixedWidth(2 * ver_spacing)
+        val_line_edit.move(ver_spacing-hor_spacing, 0)
+        val_button = QPushButton('OK', value_widget)
+        val_button.move(3*ver_spacing, 0)
+
+        self.horizontal_line(14*hor_spacing)
+
+        and_or_widget = QWidget(self)
+        and_or_widget.move(hor_spacing, 15*hor_spacing)
+        and_button = QPushButton('AND', and_or_widget)
+        or_button = QPushButton('OR', and_or_widget)
+        or_button.move(ver_spacing, 0)
+
+        self.horizontal_line(17*hor_spacing)
+
+        info_widget = QWidget(self)
+        info_widget.move(hor_spacing, 18*hor_spacing)
+        query_label = QLabel('Selected query: ', info_widget)
+        query = QLabel('abba', info_widget)
+        query.move(0, int(1.5*hor_spacing))
+        query.setStyleSheet("QLabel {color : blue;}")
+
+        self.horizontal_line(21*hor_spacing)
+
+        commit_widget = QWidget(self)
+        commit_widget.move(hor_spacing, 22*hor_spacing)
+        search_button = QPushButton('Search!', commit_widget)
+        info_label = QLabel('test', commit_widget)
+        info_label.move(ver_spacing, 4)
 
 
 
 
-    def horizontal_line(self, x, y):
+
+
+    def horizontal_line(self, y):
         horizontal_line = QWidget(self)
         horizontal_line.setFixedSize(self.width, 1)
         horizontal_line.setStyleSheet("background-color: black;")
-        horizontal_line.move(x, y)
+        horizontal_line.move(0, y)
 
 
 
